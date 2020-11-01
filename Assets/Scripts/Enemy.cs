661165fbc04a8549142b6c8e5aef3833808333d9
TTtianTT <TTtianTT@users.noreply.github.com>
2020-11-01 17:21:51 +0800

    整理文件夹，简单地实现行走

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float speed = 10;
+    private Transform[] positions;
+    private int index = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+
+    public void Move()
+    {
+        if(transform.position.x<56)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+        else
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+    }
+
+}
